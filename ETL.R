8b9878a56ea77073d20ee502a5c254707c43d34c
Thu Le <thuanhle179@gmail.com>
2017-04-25 23:02:05 -0500

    update internet usage by state

--- ETL.R
+++ ETL.R
@@ -95,10 +95,11 @@ InternetConnectivity <- data.world::query(connection = conn, type = "sql",
                                              "SELECT c.State as State, `Internet Connectivity`.`No connection anywhere (%)` as NoConnectionAnywhere, `Internet Connectivity`.`No home connection, but connect elsewhere (%)`
 as NoHomeConnection_ConnectElseWhere, `Internet Connectivity`.`Connect at home only (%)` as ConnectAtHomeOnly, 
                                           InternetUsageAtHome.`Internet Usage At Home` as InternetUsageAtHome, InternetUsageAtWork.`Internet Usage At Work` as InternetUsageAtWork,
-                                          InternetUsageAtCoffeeShops.InternetUsageAtCoffeeShops as InternetUsageAtCoffeeShops
+                                          InternetUsageAtCoffeeShops.InternetUsageAtCoffeeShops as InternetUsageAtCoffeeShops,
+                                          InternetUsage.InternetUsage
                                           FROM `Internet Connectivity.xlsx/Internet Connectivity`as c, `Internet Connectivity.xlsx/InternetUsageAtHome` as h,
-                                          InternetUsageAtCoffeeShops as s, InternetUsageAtWork as w
-                                          where c.State = h.State and s.State = h.State and h.State = w.State")
+                                          InternetUsageAtCoffeeShops as s, InternetUsageAtWork as w, InternetUsage as i
+                                          where c.State = h.State and s.State = h.State and h.State = w.State and w.State = i.State")
 df_InternetConnectivity <- data.frame(InternetConnectivity)
 # Put males into age categories
 male0to9 <- df1$B01001_003 + df1$B01001_004
@@ -135,4 +136,6 @@ df2 <- as.data.frame(cbind(State = df1$AreaName, male0to9, male10to19, male20to2
 # Change the numeric columns to numeric.
 df2[-1] <- as.data.frame(apply(df2[-1], 2, as.numeric))
 df2 <- cbind(df2, df1Second)
+
+# Do an inner join by State of df2 and InternetConnectivity
 df2 <- merge(df2,df_InternetConnectivity, by ="State")
